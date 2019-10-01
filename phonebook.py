b8c4905aa2dc7a074ed1c4c96ae9126d845ec122
tfried2001 <tfried_2001@yahoo.com>
2019-10-01 15:25:17 -0400

    Completed M04

--- phonebook.py
+++ phonebook.py
@@ -1,9 +1,11 @@
-
+import os
 
 class PhoneBook():
-
-    def __init__(self):
+    def __init__(self, cache_directory, *args, **kwargs):
         self.numbers = {}
+        self.filename = os.path.join(cache_directory, "phonebook.txt")
+        self.cache = open(self.filename, "w")
+
 
     def add(self, name, number):
         self.numbers[name] = number
@@ -11,12 +13,10 @@ class PhoneBook():
     def lookup(self, name):
         return self.numbers[name]
 
-    def is_consistent(self):
-        for name1, number1 in self.numbers.items():
-            for name2, number2 in self.numbers.items():
-                if name1 == name2:
-                    continue
-                if number1.startswith(number2):
-                    return False
-        return True
+    def names(self):
+        return set(self.numbers.keys())
+
+    def clear(self):
+        self.cache.close()
+        os.remove(self.filename)
 
--- test_phonebook.py
+++ test_phonebook.py
@@ -1,46 +1,24 @@
-import unittest
-
 from phonebook import PhoneBook
+import pytest
 
-class PhoneBookTest(unittest.TestCase):
-    def setUp(self) -> None:
-        self.phonebook = PhoneBook()
-
-    def tearDown(self) -> None:
-        pass
-
-    def test_lookup_by_name(self):
-        self.phonebook.add("Bob", "12345")
-        number = self.phonebook.lookup("Bob")
-        self.assertEqual("12345", number)
-
-    def test_missing_name(self):
-        with self.assertRaises( KeyError):
-            self.phonebook.lookup("Missing")
-
-    # @unittest.skip("WIP")
-    def test_empty_phonebook_is_consistent(self):
-        self.assertTrue(self.phonebook.is_consistent())
-
-    def test_is_consistent_with_different_entries(self):
-        self.phonebook.add("Bob", "12345")
-        self.phonebook.add("Sue", "012345")
-        self.assertTrue(self.phonebook.is_consistent())
-
-    def test_inconsistent_with_duplicate_entries(self):
-        self.phonebook.add("Bob", "12345")
-        self.phonebook.add("Sue", "12345")
-        self.assertFalse(self.phonebook.is_consistent())
-
-    def test_inconsistent_with_duplicate_prefix(self):
-        self.phonebook.add("Bob", "12345")
-        self.phonebook.add("Sue", "123")
-        self.assertFalse(self.phonebook.is_consistent())
-
-    @unittest.skip("WIP")
-    def test_phonebook_adds_names_and_numbers(self):
-        self.phonebook.add("Sue", "123343")
-        self.assertIn("Sue", self.phonebook.get_names())
-        self.assertIn("123343", self.phonebook.get_numbers())
+@pytest.fixture
+def phonebook(tmpdir):
+    "Provides an empty Phonebook object"
+    phonebook = PhoneBook(tmpdir)
+    yield phonebook
+    phonebook.clear()
 
 
+def test_lookup_by_name(phonebook):
+    phonebook.add("Bob", "1234")
+    assert "1234" == phonebook.lookup("Bob")
+
+
+def test_phonebook_contains_all_names(phonebook):
+    phonebook.add("Bob", "1234")
+    assert "Bob" in phonebook.names()
+
+
+def test_missing_name_raises_error(phonebook):
+    with pytest.raises(KeyError):
+        phonebook.lookup("Bob")
