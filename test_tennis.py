d5e756368088f1f93c82ca22b5233516d050f43e
tfried2001 <tfried_2001@yahoo.com>
2019-10-07 09:44:51 -0400

    Finish M07L02

--- test_tennis.py
+++ test_tennis.py
@@ -1,11 +1,16 @@
 from tennis import score_tennis
 
-def test_0_0_love_all():
-    assert score_tennis(0, 0) == "Love All"
+import pytest
 
-def test_1_1_fifteen_all():
-    assert score_tennis(1, 1) == "Fifteen All"
 
-def test_2_2_thirty_all():
-    assert score_tennis(2, 2) == "Thirty All"
+@pytest.mark.parametrize("player1_points, player2_points, expected_score",
+                        [(0,0, "Love-All"),
+                        (1,1,"Fifteen-All"),
+                        (2,2,"Thirty-All"),
+                        (0,2,"Love-Thirty"),
+                        (1,2,"Fifteen-Thirty"),
+                        (2,4,"Win for Player 2"),
+                        ])
+def test_score_tenis(player1_points, player2_points, expected_score):
+    assert score_tennis(player1_points, player2_points) == expected_score
 
