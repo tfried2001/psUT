4cb4c4d267c72284884f5708a8bd6164024d030e
tfried2001 <tfried_2001@yahoo.com>
2019-10-07 09:57:26 -0400

    Finished M07L03 unittest parametrized

--- test_tennis.py
+++ test_tennis.py
@@ -1,16 +1,29 @@
+import unittest
 from tennis import score_tennis
 
-import pytest
 
 
-@pytest.mark.parametrize("player1_points, player2_points, expected_score",
-                        [(0,0, "Love-All"),
-                        (1,1,"Fifteen-All"),
-                        (2,2,"Thirty-All"),
-                        (0,2,"Love-Thirty"),
-                        (1,2,"Fifteen-Thirty"),
-                        (2,4,"Win for Player 2"),
-                        ])
-def test_score_tenis(player1_points, player2_points, expected_score):
-    assert score_tennis(player1_points, player2_points) == expected_score
+class TennisTest(unittest.TestCase):
+
+    def test_score_tennis(self):
+        test_cases = [
+            (0,0, "Love-All"),
+            (0,1,"Fifteen-All"),
+            (2,2,"Thirty-All"),
+            (0,2,"Love-Thirty"),
+            (1,2,"Fifteen-Thirty"),
+            (2,4,"Win for Player 2"),
+        ]
+        for player1_points, player2_points, expected_score in test_cases:
+            with self.subTest(f"{player1_points}, {player2_points} -> {expected_score}"):
+                self.assertEqual(expected_score, score_tennis(player1_points, player2_points))
+
+    def test_0_0_love_all(self):
+        self.assertEqual("Love-All", score_tennis(0, 0))
+
+    def test_1_1_fifteen_all(self):
+        self.assertEqual("Fifteen-All", score_tennis(1, 1))
+
+    def test_2_2_thirty_all(self):
+        self.assertEqual("Thirty-All", score_tennis(2, 2))
 
